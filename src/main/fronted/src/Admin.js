8c7376b769be7f76d24926faa66f7a5ad365aa0d
suubinkim <0vbbbv0@gmail.com>
2024-12-26 08:57:40 +0900

    홈 이동 버튼 추가

--- src/main/fronted/src/Admin.js
+++ src/main/fronted/src/Admin.js
@@ -1,13 +1,24 @@
 import {useEffect, useState} from "react";
 import fetchAuthorizedPage from "./fetchAuthorizedPage";
+import {useNavigate} from "react-router-dom";
 
 const Admin = () => {
+    const navigate = useNavigate();
     const [data, setData] = useState("");
     useEffect(() => {
         fetchAuthorizedPage("http://localhost:8011/admin")
             .then(result => setData(result));
     }, []);
-    return data && <h2>{data}</h2>;
+    return <div>
+        {data && <h2>{data}</h2>}
+        <button
+            type="button"
+            className="navyBtn"
+            onClick={() => navigate('/')}
+        >
+            Home
+        </button>
+    </div>
 }
 
 export default Admin;
